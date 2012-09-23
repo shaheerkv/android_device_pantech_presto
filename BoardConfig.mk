cccf75af054c2c0ccef1175a98ae8a7b356f5677
mifl <mifl.pc@gmail.com>
2012-09-23 22:48:38 +0300

    Update BoardConfig.mk
    Generic config msm8660

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,66 @@
 USE_CAMERA_STUB := true
+USE_PROPRIETARY_AUDIO_EXTENSIONS := false
 
 # inherit from the proprietary version
 -include vendor/pantech/presto/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := presto
+
+TARGET_USES_OVERLAY := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+TARGET_HAVE_HDMI_OUT := false
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8660
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/pantech/presto/egl.cfg
+
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm8660
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := false
+
+# Wifi
+BOARD_WLAN_DEVICE := bcm4329
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+DYNAMIC_SHARED_LIBV8SO  := false
+
+# Scorpion optimizations
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_USE_SCORPION_PLD_SET := true
+# Set to 9 for 8650A
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom loglevel=0
 BOARD_KERNEL_BASE := 0x40200000
@@ -22,6 +76,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/pantech/presto/kernel
 
-#BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
-#BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_LARGE_FILESYSTEM := true
