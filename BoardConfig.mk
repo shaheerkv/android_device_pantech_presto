cc46897930ef159c6a857a71e73b58e8f7995e27
MIFL <mifl.pc@gmail.com>
2012-12-17 14:41:37 +0100

    wifi revision

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,18 +107,18 @@ TARGET_FORCE_CPU_UPLOAD := true
 DYNAMIC_SHARED_LIBV8SO := false
 
 # Wifi related defines
-BOARD_WLAN_DEVICE                := bcm4329
-BOARD_WLAN_DEVICE_REV            := bcm4329
-BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-WIFI_BAND                        := 802_11_ABG
-WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/etc/wl/nvram.tx iface_name=wlan"
-WIFI_DRIVER_MODULE_NAME          := "wlan"
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                   := bcm4329
+BOARD_WLAN_DEVICE_REV               := bcm4329
+BOARD_WPA_SUPPLICANT_DRIVER         := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_wext
+WIFI_BAND                           := 802_11_ABG
+WIFI_DRIVER_FW_PATH_AP              := "/system/etc/wl/bcm43291_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM           := "/sys/module/wlan/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA             := "/system/etc/wl/bcm43291.bin"
+WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wl/bcm43291.bin nvram_path=/system/etc/wl/nvram.txt"
+WIFI_DRIVER_MODULE_NAME             := "wlan"
+WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
+WPA_SUPPLICANT_VERSION              := VER_0_8_X
 
 #----------------------------------------------------------------------
 
