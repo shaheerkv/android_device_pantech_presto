3e8760990e70cf500f200b548dc21c30bb76db21
jied83 <jied83@gmail.com>
2013-01-20 02:29:50 +0400

    wifi fix
    Change-Id: I2216444b87d44feab45145ce7fd09246b729562d

--- wifi.mk
+++ wifi.mk
@@ -1,17 +1,17 @@
 # Wifi related defines
-BOARD_HOSTAPD_DRIVER                := WEXT
-BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_wext
+#BOARD_HOSTAPD_DRIVER                := WEXT
+#BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_wext
 BOARD_WLAN_DEVICE                   := bcm4329
 #BOARD_WLAN_DEVICE_REV               := bcm4329
 BOARD_WPA_SUPPLICANT_DRIVER         := WEXT
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_wext
 WIFI_BAND                           := 802_11_ABG
-WIFI_DRIVER_FW_PATH_AP              := "/vendor/firmware/fw_bcm4329_apsta.bin"
-WIFI_DRIVER_FW_PATH_PARAM           := "/sys/module/bcm4329/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA             := "/vendor/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_MODULE_ARG              := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/vendor/firmware/nvram_net.txt iface_name=wlan"
+WIFI_DRIVER_FW_PATH_AP              := "/system/vendor/firmware/bcm43291_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM           := "/sys/module/wlan/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA             := "/system/vendor/firmware/bcm43291.bin"
+WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/vendor/firmware/bcm43291.bin nvram_path=/system/vendor/firmware/nvram.txt"
 WIFI_DRIVER_MODULE_NAME             := "wlan"
 WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
 WPA_SUPPLICANT_VERSION              := VER_0_8_X
-#WIFI_EXT_MODULE_PATH        	    := "/system/lib/modules/librasdioif.ko"
-#WIFI_EXT_MODULE_NAME        	    := "librasdioif"
+WIFI_EXT_MODULE_PATH                := "/system/lib/modules/librasdioif.ko"
+WIFI_EXT_MODULE_NAME                := "librasdioif"
