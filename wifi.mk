e88ef524e1cfeb57c7c574c2a838c98771cbd7c1
jied83 <jied83@gmail.com>
2013-01-20 13:57:11 +0400

    correction 3/3
    Change-Id: Ie8e15b140b085f4e97542f083f65907e9b1ef8ff

--- wifi.mk
+++ wifi.mk
@@ -6,10 +6,10 @@ BOARD_WLAN_DEVICE                   := bcm4329
 BOARD_WPA_SUPPLICANT_DRIVER         := WEXT
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_wext
 WIFI_BAND                           := 802_11_ABG
-WIFI_DRIVER_FW_PATH_AP              := "/system/vendor/firmware/bcm43291_apsta.bin"
+WIFI_DRIVER_FW_PATH_AP              := "/system/etc/wl/bcm43291_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM           := "/sys/module/wlan/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA             := "/system/vendor/firmware/bcm43291.bin"
-WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/vendor/firmware/bcm43291.bin nvram_path=/system/vendor/firmware/nvram.txt"
+WIFI_DRIVER_FW_PATH_STA             := "/system/etc/wl/bcm43291.bin"
+WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wl/bcm43291.bin nvram_path=/system/etc/wl/nvram.txt"
 WIFI_DRIVER_MODULE_NAME             := "wlan"
 WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
 WPA_SUPPLICANT_VERSION              := VER_0_8_X
