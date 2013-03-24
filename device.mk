5145ce0b39c79cf9be78a80d0047c839ffbf6a0e
jied83 <jied83@gmail.com>
2013-03-24 19:20:13 +0400

    proximity+offline charge+move to sd function fixes
    Change-Id: I2c325c921f634c5954b4961fefb8e7dffced80dc

--- device.mk
+++ device.mk
@@ -68,6 +68,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.presto.rc:root/ueventd.rc
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.pantech.usb.rc:root/init.pantech.usb.rc
+
 # Wifi (bcmdhd)
 #WIFI_BAND := 802_11_ABG
 #$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
@@ -76,7 +79,8 @@ PRODUCT_COPY_FILES += \
 
 # FM Radio
 PRODUCT_PACKAGES += FmRadioReceiver
-
+# Camera
+PRODUCT_PACKAGES += Camera
 # Ramdisk
 PRODUCT_PACKAGES += init.presto.rc
 
