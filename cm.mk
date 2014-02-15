20434d9526e6078c1cdb99fa09194609c92b141e
MIFL <mifl.pc@gmail.com>
2014-02-15 07:41:49 +0100

    Add CyanogenMod prebuilt bootanimation
    Change-Id: Ia3fb1c4398cca20ddd06f207087d7605d84db55b

--- cm.mk
+++ cm.mk
@@ -10,6 +10,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/pantech/presto/full_presto.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH  := 480
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_presto
 PRODUCT_DEVICE := presto
