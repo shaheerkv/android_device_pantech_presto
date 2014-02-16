b070dbf456504c202d21595abad6c1fb0c7ba9ae
MIFL <mifl.pc@gmail.com>
2014-02-16 20:29:45 +0100

    presto: update wifi to build cm-11.0
    Change-Id: Ic06be0ee674e299a415109dfe6a19b0247b2b5b4

--- wifi/device-bcm.mk
+++ wifi/device-bcm.mk
@@ -1,4 +1,11 @@
 
+# Wifi config
+PRODUCT_COPY_FILES += \
+    device/pantech/presto/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+    device/pantech/presto/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+
+#----------------------------------------------------------------------
+
 # Wifi (bcmdhd)
 #WIFI_BAND := 802_11_ABG
 #$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
