83182f9a3e4461cbe54aea4b96d07d12f42a2296
MIFL <mifl.pc@gmail.com>
2014-02-16 20:35:55 +0100

    presto: update wifi bcm4329 config
    Change-Id: Id70b42b0b1a364215e48ea5277816c1e6ddb2962

--- wifi/board-bcm.mk
+++ wifi/board-bcm.mk
@@ -1,18 +1,25 @@
 # Wifi related defines
-#BOARD_HOSTAPD_DRIVER               := WEXT
-#BOARD_HOSTAPD_PRIVATE_LIB          := lib_driver_cmd_wext
+
+BOARD_WPA_SUPPLICANT_DRIVER         := WEXT
+WPA_SUPPLICANT_VERSION              := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_wext
+BOARD_HOSTAPD_DRIVER                := WEXT
+BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_wext
 BOARD_WLAN_DEVICE                   := bcm4329
 #BOARD_WLAN_DEVICE_REV              := bcm4329
-BOARD_WPA_SUPPLICANT_DRIVER         := WEXT
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_wext
 WIFI_BAND                           := 802_11_ABG
-WIFI_DRIVER_FW_PATH_AP              := "/system/etc/wl/bcm43291_apsta.bin"
+
+WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME             := "wlan"
+WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wl/bcm43291.bin nvram_path=/system/etc/wl/nvram.txt"
+WIFI_DRIVER_MODULE_AP_ARG           := "firmware_path=/system/etc/wl/bcm43291_apsta.bin nvram_path=/system/etc/wl/nvram.txt"
 WIFI_DRIVER_FW_PATH_PARAM           := "/sys/module/wlan/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA             := "/system/etc/wl/bcm43291.bin"
-WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wl/bcm43291.bin nvram_path=/system/etc/wl/nvram.txt"
-WIFI_DRIVER_MODULE_NAME             := "wlan"
-WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
-WPA_SUPPLICANT_VERSION              := VER_0_8_X
+WIFI_DRIVER_FW_PATH_AP              := "/system/etc/wl/bcm43291_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P             := "/system/etc/wl/bcm43291_p2p.bin"
+
 WIFI_EXT_MODULE_PATH                := "/system/lib/modules/librasdioif.ko"
 WIFI_EXT_MODULE_NAME                := "librasdioif"
+
+BOARD_HAVE_PRESTO_WIFI              := true
 TARGET_CUSTOM_WIFI                  := ../../device/pantech/presto/wifi/presto_wifi.c
