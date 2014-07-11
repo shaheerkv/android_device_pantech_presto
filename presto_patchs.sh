f26748444f0b371e9376264d5978c14b0a5881a5
jied83 <jied83@gmail.com>
2014-07-11 15:58:49 +0400

    presto_patchs: add patches, fix broken patches
    Change-Id: Ic10775de2ecc032578134de391a156d3e8e28338

--- presto_patchs.sh
+++ presto_patchs.sh
@@ -9,12 +9,22 @@ while getopts ":ar" option;
 do
     case $option in
         a)
-            ## patch CameraParameters
+            echo "*** Patch for camera ***" 
             patch -p1 < $PATCH_DIR/cameraparameters.patch
             ## Patch PowerManagerService.java
+            echo "*** Patch for proximity sensor ***" 
             patch -p1 < $PATCH_DIR/proximity_sensor.patch
             ## Patch libhardware_legacy
-            patch -p1 < $PATCH_DIR/custom_wific.patch
+            #patch -p1 < $PATCH_DIR/custom_wific.patch
+            echo "*** Patch for wifi.c ***" 
+            cd hardware/libhardware_legacy/wifi
+            patch -p2 < $CM_DIR/$PATCH_DIR/custom_wific.patch
+            echo "*** Patch for bcm4329 ***" 
+            cd ../../broadcom/wlan
+            patch -p1 < ../../../$PATCH_DIR/bcm4329.patch
+            echo "*** Patch for wpa_supplicant_8 ***" 
+            cd ../../../external/wpa_supplicant_8
+            patch -p1 < ../../$PATCH_DIR/wpa_supplicant_8.patch
             ;;
         r)
             ## patch CameraParameters
@@ -22,10 +32,21 @@ do
             ## Patch PowerManagerService.java
             patch -Rp1 < $PATCH_DIR/proximity_sensor.patch
             ## Patch libhardware_legacy
-            patch -Rp1 < $PATCH_DIR/custom_wific.patch
-            ;;
+            #patch -Rp1 < $PATCH_DIR/custom_wific.patch
+            cd hardware/libhardware_legacy/wifi
+            patch -Rp2 < $CM_DIR/$PATCH_DIR/custom_wific.patch
+            cd ../../broadcom/wlan
+            patch -Rp1 < ../../../$PATCH_DIR/bcm4329.patch
+            cd ../../../external/wpa_supplicant_8
+            patch -Rp1 < ../../$PATCH_DIR/wpa_supplicant_8.patch
+           ;;
+   
         *)
             echo "invalid option -$OPTARG" 
+            echo "*****************************"
+            echo "-a apply patches"
+            echo "-r revert applied patches"
+            echo "*****************************"
             ;;
     esac
 done
